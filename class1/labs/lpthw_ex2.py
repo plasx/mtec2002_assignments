32fba2cd59f64d9c6b6c402bfe27fb3ec01b2267
plasx <plasx@live.com>
2012-02-01 19:49:21 -0500

    um i just wrote what i saw and it worked, yay im soooo happpy >:D first this then the world >:D

---
+++ class1/labs/lpthw_ex2.py
@@ -0,0 +1,8 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "i could have code like this. " # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of dcode:# print "this won't run"
+
+print" this will run"
