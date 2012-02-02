01c951de80b631818c5aa4f79b2812dcb691e177
plasx <plasx@live.com>
2012-02-01 20:18:34 -0500

    man its like we typed so much stuff here and the print out example is like so smallll!!

---
+++ class1/labs/lpthw_ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Daniel P. Rivera'
+my_age = 24 # not a lie >:D
+my_height =74 # inches
+my_weight = 228 # Lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "he's got %s eyes and %s hair." % (my_eyes , my_hair)
+print "His teeth are usually %s dpeneding on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactkly right
+print "If i add %d, %d, and %d I get %d." %(
+my_age, my_height, my_age, + my_height + my_weight)
